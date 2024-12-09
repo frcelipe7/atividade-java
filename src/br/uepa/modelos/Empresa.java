80231c3d10b12fd0152cf3c332112bf16216f44f
Felipe Raphael Coelho dos Santos <feliperaphaelpara@gmail.com>
2024-12-09 19:24:41 -0300

    mudando hierarquia

--- src/br/uepa/modelos/Empresa.java
+++ src/br/uepa/modelos/Empresa.java
@@ -3,23 +3,25 @@ package br.uepa.modelos;
 public class Empresa {
     private String nomeEmpresa;
     private String cnpjEmpresa;
-    private Predio predio;
 
-    public Empresa(String nomeEmpresa, String cnpjEmpresa, String nomeGerente) {
+    public Empresa(String nomeEmpresa, String cnpjEmpresa) {
         this.nomeEmpresa = nomeEmpresa;
         this.cnpjEmpresa = cnpjEmpresa;
-        this.predio = new Predio(nomeGerente);
     }
 
-    public void setEndereco(String cep, String estado, String cidade, String bairro, String rua, int numero) {
-        this.predio.setEndereco(cep, estado, cidade, bairro, rua, numero);
+    public void setNomeEmpresa(String nomeEmpresa) {
+        this.nomeEmpresa = nomeEmpresa;
     }
 
-    public String getEstado() {
-        return this.predio.endereco.getEstado();
+    public void setCnpjEmpresa(String cnpjEmpresa) {
+        this.cnpjEmpresa = cnpjEmpresa;
     }
 
-    public void verificar() {
-        System.out.println(String.format("Nome: %s, cnpj: %s, nome gerente: %s", this.nomeEmpresa, this.cnpjEmpresa, this.predio.getNomeGerente()));
+    public String getNomeEmpresa() {
+        return this.nomeEmpresa;
+    }
+
+    public String getCnpjEmpresa() {
+        return this.cnpjEmpresa;
     }
 }
