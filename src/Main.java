a86d3f83e515ce37980428318eb77128607b7062
Felipe Raphael Coelho dos Santos <feliperaphaelpara@gmail.com>
2024-12-11 16:10:34 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,10 @@ public class Main {
         final String ANSI_GREEN = "\u001B[32m";
         final String ANSI_BLUE = "\u001B[34m";
 
-        System.out.println("\n===============================================");
+        System.out.println("\n================================================");
         System.out.println("================= Empresas.com =================");
         System.out.println("================================================");
+        System.out.println("Alguns direitos reservados");
         System.out.println("v1.4.53\n");
 
         for (int i = 0; i < 50; i++) {
@@ -22,9 +23,12 @@ public class Main {
             System.out.print("█");
         }
 
-        System.out.print(ANSI_GREEN+"\r100%"+ANSI_RESET);
+        Thread.sleep(100);
+
+        System.out.print(ANSI_GREEN+"\r100%                                                            "+ANSI_RESET);
         Thread.sleep(1000);
         System.out.print(ANSI_GREEN+"\rBem-vindo(a) ao melhor sistema empresarial do mundo!\n\n"+ANSI_RESET);
+
 
         Thread.sleep(2000);
 
@@ -66,8 +70,14 @@ public class Main {
                     String nomeGerente = userInput.nextLine();
                     minhaRevendedora.setNomeGerente(nomeGerente);
 
+                    System.out.println("\n\nExibindo informacoes da loja ==============");
+
+
                     System.out.println("Nome gerente: " + minhaRevendedora.getNomeGerente());
                     System.out.println("Nome empresa: " + minhaRevendedora.getEmpresa().getNomeEmpresa());
+
+                    System.out.print("\n");
+                    Thread.sleep(500);
 
                     System.out.println(ANSI_BLUE + "Produtos no estoque da loja: " + ANSI_RESET + minhaRevendedora.getQtdProdutosEstoque());
 
@@ -77,16 +87,22 @@ public class Main {
                     System.out.println("Adicionando 100 produtos no estoque");
                     System.out.println(ANSI_BLUE + "Produtos no estoque da loja: " + ANSI_RESET + minhaRevendedora.getQtdProdutosEstoque());
 
+                    System.out.print("\n");
+                    Thread.sleep(500);
+
                     // venda realizada com sucesso
-                    minhaRevendedora.saidaEstoque(80);
                     System.out.println("\nRealizando venda ==============");
                     System.out.println("Venda de 80 produtos");
+                    minhaRevendedora.saidaEstoque(80);
                     System.out.println(ANSI_BLUE + "Produtos no estoque da loja: " + ANSI_RESET + minhaRevendedora.getQtdProdutosEstoque());
 
+                    System.out.print("\n");
+                    Thread.sleep(500);
+
                     // quantidade no estoque é insuficiente
-                    minhaRevendedora.saidaEstoque(80);
                     System.out.println("\nRealizando venda ==============");
                     System.out.println("Venda de 80 produtos");
+                    minhaRevendedora.saidaEstoque(80);
                     System.out.println(ANSI_BLUE + "Produtos no estoque da loja: " + ANSI_RESET + minhaRevendedora.getQtdProdutosEstoque());
 
                     break;
@@ -107,46 +123,68 @@ public class Main {
                     String nomeGerenteFabrica = userInput.nextLine();
                     minhaFabrica.setNomeGerente(nomeGerenteFabrica);
 
+                    System.out.println("\n\nExibindo informacoes da loja ==============");
+
                     System.out.println(ANSI_BLUE + "Nome gerente: " + ANSI_RESET + minhaFabrica.getNomeGerente());
                     System.out.println(ANSI_BLUE + "Nome empresa: " + ANSI_RESET + minhaFabrica.getEmpresa().getNomeEmpresa());
+
+                    System.out.print("\n");
+                    Thread.sleep(500);
 
                     System.out.println(ANSI_BLUE + "Produtos no estoque da loja: " + ANSI_RESET + minhaFabrica.getQtdProdutosEstoque());
 
                     // fabricando e estocando produtos
-                    minhaFabrica.fabricarProdutos(100);
                     System.out.println("\nFabricando produtos ==============");
                     System.out.println("Fabricando 100 produtos");
+                    minhaFabrica.fabricarProdutos(100);
                     System.out.println(ANSI_BLUE + "Produtos fabricados e estocados: " + ANSI_RESET + minhaFabrica.getQtdProdutosEstoque());
 
+                    System.out.print("\n");
+                    Thread.sleep(500);
+
                     // enviando produtos para a revendedora
-                    minhaFabrica.saidaEstoque(80);
                     System.out.println("\nRealizando venda ==============");
                     System.out.println("Realizando venda de 80 produtos");
+                    minhaFabrica.saidaEstoque(80);
                     System.out.println(ANSI_BLUE + "Produtos no estoque da loja: " + ANSI_RESET + minhaFabrica.getQtdProdutosEstoque());
 
+                    System.out.print("\n");
+                    Thread.sleep(500);
+
                     // quantidade no estoque é insuficiente
-                    minhaFabrica.saidaEstoque(80);
                     System.out.println("\nRealizando venda ==============");
                     System.out.println("Realizando venda de 80 produtos");
+                    minhaFabrica.saidaEstoque(80);
                     System.out.println(ANSI_BLUE + "Produtos no estoque da loja: " + ANSI_RESET + minhaFabrica.getQtdProdutosEstoque());
                     break;
                 case 3:
                     System.out.println(ANSI_RED+"Encerrando sistema :("+ANSI_RESET);
                     break;
             }
+            System.out.print("\n");
+            Thread.sleep(500);
+
             System.out.println("O que voce deseja fazer?\n [1] - Criar loja;\n [2] - Criar fabrica;\n [3] - Sair;");
             op = userOp.nextInt();
         }
 
+        for (int i = 0; i < 30; i++) {
+            Thread.sleep(50);
+            System.out.print("-");
+        }
+        System.out.println("\nGrandiosos desenvolvedores do melhor sistema para empresas do mundo: \"Empresas.com:\"");
 
-        System.out.println(ANSI_GREEN+"\n\nGrandioso desenvolvedores do melhor sistema para empresas Empresas.com:"+ANSI_RESET);
-
-        System.out.println(ANSI_RED+"Felipe Raphael Coelho dos Santos");
+        System.out.println(ANSI_GREEN+"Felipe Raphael Coelho dos Santos");
+        Thread.sleep(200);
         System.out.println("Vinícius Mendes da Silva Correa");
+        Thread.sleep(200);
         System.out.println("Rosival Soares Peres Junior");
+        Thread.sleep(200);
         System.out.println("Jainer Silva Leite"+ANSI_RESET);
-        System.out.println("===========================");
-
-
+        Thread.sleep(200);
+        for (int i = 0; i < 30; i++) {
+            Thread.sleep(50);
+            System.out.print("-");
+        }
     }
 }
